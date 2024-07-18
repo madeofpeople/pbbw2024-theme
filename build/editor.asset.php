9161b448058cca5a7d4929ff70f1d333babc5380
Thiago de Mello Bueno <thiago@madeofpeople.org>
2024-07-17 20:23:25 -0400

    prelims

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '08f7371a217df185f394');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9d1ede0853c052d4d8a4');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '9c3cee4ea3d7d0fbaebd');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '268ff4d2c1983efb22f6');
