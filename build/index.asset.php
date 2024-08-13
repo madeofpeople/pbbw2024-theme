7811890e44c3f7d848e3816c476cad3905a96a82
Thiago de Mello Bueno <thiago@madeofpeople.org>
2024-08-13 16:53:57 -0400

    theme updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '1fd9491e928ed1908a13');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '6674907dbf1fff602e87');
