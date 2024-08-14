e5f7f08c229f78f5b0fba551278d6326d9ad571d
Thiago de Mello Bueno <thiago@madeofpeople.org>
2024-08-14 18:58:52 -0400

    fancy button

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '87d5f4f42de1d28ffb90');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'd91cec97d65a0c4126c2');
