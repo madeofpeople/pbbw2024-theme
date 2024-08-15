1ab6e66c6ed57ad08f9c139586deb70113b4d917
Thiago de Mello Bueno <thiago@madeofpeople.org>
2024-08-15 13:40:08 -0400

    min height for mobile sizes of shop tout

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'e07feb317832b18db6b4');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'ab98d154eb549ad0ee8c');
