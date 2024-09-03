62d1b527f8f4b910d199d41713c69798bf982280
Thiago de Mello Bueno <thiago@madeofpeople.org>
2024-09-03 13:00:14 -0400

    remove breakpoint indicator

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => 'bcff856b34a58e2a93d0');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '67ded119be9687fbe989');
